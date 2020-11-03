a861730a4447d3f4d08c5d954a25dae7742d2633
limhawjia <hawjiaa@gmail.com>
2020-11-03 18:46:58 +0800

    Finish endpoint

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+// DB connections
+const { Pool } = require('pg');
+const pool = new Pool();
+
+// Middleware for routers
 const router = express.Router();
 router.use(cookieParser());
+router.use(express.json());
+
 
 const LEVEL_1_PROMO_CODE = "`2MH}\?TV<@4M;T{&CD?3'YH";
 
+
+
+// Level 1 endpoint
 router.get('/admin/promo', (req, res) => {
   const cookie = req.cookies['session'];
 
   if (!cookie) {
-    res.status(401).send("Not authorized");
+    res.status(401).send("Not authorized!");
+    return;
   }
 
   const buff = Buffer.from(cookie, 'base64');
@@ -22,6 +33,31 @@ router.get('/admin/promo', (req, res) => {
   } else {
     res.status(401).send("Not authorized!");
   }
+});
+
+
+// Level 2 endpoint
+router.post('/login', (req, res) => {
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send("Bad request!");
+    return;
+  }
+
+  const { username, password } = req.body;
+
+  query = `SELECT id, username FROM users WHERE username = '${username}' AND pw = '${password}'`;
+  
+  pool.query(query)
+    .then(({rows}) => {
+      if (rows.length === 0) {
+        res.status(400).send("Invalid credentials!");
+      }
+      
+      res.status(200).send(rows[0]);
+    })
+    .catch(e => {
+       res.status(500).send(`Server error: ${e.message}`);
+    })
 });
 
 module.exports = router;
