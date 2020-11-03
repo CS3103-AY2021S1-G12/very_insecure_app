cab81e7146dfce6d8248a9ce80f3f8be9a27d8ad
Shawn Chew <shawnpchew@gmail.com>
2020-11-03 16:44:11 +0800

    add basic schema for db

---
+++ src/db/schema.sql
@@ -0,0 +1,35 @@
+-- SCHEMA FOR USERS
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER,
+    user VARCHAR,
+    pw VARCHAR,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO users(id, user, pw) VALUES (1, 'admin', 'welove3103');
+INSERT INTO users(id, user, pw) VALUES (1, 'bob', 'qwerty');
+INSERT INTO users(id, user, pw) VALUES (1, 'charlie', 'password');
+
+-- SCHEMA FOR E-COMMERCE ITEM
+CREATE TABLE IF NOT EXISTS products (
+    id INTEGER,
+    item_name VARCHAR,
+    price NUMBER,
+    promo_code VARCHAR,
+    internal_comments VARCHAR,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO products(id, item_name, price, promo_code, internal_comments) 
+    VALUES (1, 'Fan', 10.5, '3103ISTHEBEST', '');
+INSERT INTO products(id, item_name, price, promo_code, internal_comments) 
+    VALUES (2, 'Wardrobe', 5.5, '3103ISTHEBEST', '');
+
+-- SCHEMA FOR REVIEWS
+CREATE TABLE IF NOT EXISTS reviews(
+    id INTEGER,
+    user_id INTEGER, 
+    review VARCHAR,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
