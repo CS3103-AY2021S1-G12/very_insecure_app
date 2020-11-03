e784f66a9a14e761a067c77a1f7220c51526a868
Huang Weijie <h_weijie@yahoo.com>
2020-11-03 17:22:40 +0800

    fix stuff

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const { Pool } = require('pg');
 
-<<<<<<< HEAD
-// const pg = new Client();
-// pg.connect();
-=======
 const pool = new Pool();
 pool.query('SELECT 1;', (err, res) => {
     console.log(err, res);
     pool.end();
 });
->>>>>>> main
 
 const app = express();
 const port = 3000;
@@ -20,9 +15,9 @@ const authBypassRouter = require('./src/api/auth');
 app.use(authBypassRouter);
 
 app.get('*', (req, res) => {
-  res.status(404).send("Not found!");
+    res.status(404).send("Not found!");
 });
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+    console.log(`Listening on port ${port}`)
 });
