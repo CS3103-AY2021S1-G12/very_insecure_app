81eb4227279c37c99dc96472a3145421876d823c
limhawjia <hawjiaa@gmail.com>
2020-11-03 17:23:03 +0800

    Fix conflict

--- server.js
+++ server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const { Pool } = require('pg');
 
-<<<<<<< HEAD
-// const pg = new Client();
-// pg.connect();
-=======
 const pool = new Pool();
 pool.query('SELECT 1;', (err, res) => {
     console.log(err, res);
     pool.end();
 });
->>>>>>> main
 
 const app = express();
 const port = 3000;
