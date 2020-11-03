604270e36f4c30785559b14cfd575ec9eae9ec66
Huang Weijie <h_weijie@yahoo.com>
2020-11-03 16:51:38 +0800

    add app to docker compose

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const pg = new Client();
-pg.connect();
+const pool = new Pool();
+pool.query('SELECT 1;', (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
 
 const app = express();
 const port = 3000;
