e910762431aeefdee9ed7ebea4c40ccdbaacbb43
Huang Weijie <h_weijie@yahoo.com>
2020-11-03 19:27:52 +0800

    update stuffz

--- src/xxs-1/index.js
+++ src/xxs-1/index.js
@@ -1,6 +1,17 @@
+let done = false;
+
 $(document).ready(() => {
     $('#submit').click(() => {
         const inputText = $('#input').val();
         $('#reviews').append(`<li>${inputText}</li><br>`);
-    })
+    });
+
+    const originalAlert = window.alert;
+    window.alert = (msg) => {
+        if (!done) {
+            $('#success').append(`<button>Next level</button>`);
+            done = true;
+        }
+        originalAlert('Good job!');
+    }
 });
