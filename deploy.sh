2cb512b2c2a1ca34893e00b0dbead9c579ed0bc5
Huang Weijie <h_weijie@yahoo.com>
2020-11-04 11:36:09 +0800

    add deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+set -o errexit
+set -o pipefail
+
+gcloud compute instances start very-insecure-app --zone asia-southeast1-b
+IP=$(gcloud compute instances describe very-insecure-app \
+    --zone='asia-southeast1-b'\
+    --format='get(networkInterfaces[0].accessConfigs[0].natIP)')
+
+gcloud compute ssh very-insecure-app \
+    --zone asia-southeast1-b\
+    --command 'rm -rf ~/app \
+            && git clone https://github.com/CS3103-AY2021S1-G12/very_insecure_app.git ~/app \
+            && cd ~/app \
+            && docker-compose up --build -d'
+
+if [[ -z ${DUCKDNS_TOKEN} ]]; then
+    echo "DuckDNS token not set, deployed without updating DNS records."
+else
+    curl "https://www.duckdns.org/update?domains=very-secure-app&token=${DUCKDNS_TOKEN}&ip=${IP}&verbose=true"
+fi
