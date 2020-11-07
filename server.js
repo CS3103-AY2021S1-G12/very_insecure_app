f8228a47e608b5c07a4b89b438b63bf774079308
Huang Weijie <h_weijie@yahoo.com>
2020-11-07 22:53:24 +0800

    tidy build process and fix minor bugs

--- server.js
+++ server.js
@@ -7,39 +7,44 @@ const cors = require('cors');
 const path = require('path');
 
 const pool = new Pool();
-pool.query('SELECT 1;', (err, res) => {
-    console.log({ err, res });
+pool.query('SELECT 1;', (err, _) => {
+    if (err) {
+        console.log({ err })
+    } else {
+        console.log('db connection ok');
+    }
     pool.end();
 });
 
 const app = express();
 const port = 3000;
-const staticDir = './src/static';
-
-const levelsRouter = require('./src/api/levelSystem');
-app.use('/levels', levelsRouter);
-
-const staticRouter = serveStatic(path.join(__dirname, staticDir));
-app.use('/levels', staticRouter);
-
+const staticDir = path.resolve(__dirname, 'dist');
+const index = path.resolve(staticDir, 'index.html');
 app.use(cors());
 app.use(cookieParser())
+
+// const levelsRouter = require('./src/api/levelSystem');
+// app.use('/levels', levelsRouter);
+const cookie = "eyJ1c2VybmFtZSI6Ikd1ZXN0IiwiaXNBZG1pbiI6ZmFsc2UsImlzQXV0aGVudGljYXRlZCI6dHJ1ZSwidG9rZW4iOiJqSmxFcHokV0c0M2sifQ==";
+app.get('*', (req, res, next) => {
+    res.cookie('session', cookie, { httpOnly: true });
+    next();
+})
+
+const staticRouter = serveStatic(staticDir);
+app.use('/', staticRouter);
 
 const authBypassRouter = require('./src/api/auth');
 app.use(authBypassRouter);
 
-const cookie = "eyJ1c2VybmFtZSI6Ikd1ZXN0IiwiaXNBZG1pbiI6ZmFsc2UsImlzQXV0aGVudGljYXRlZCI6dHJ1ZSwidG9rZW4iOiJqSmxFcHokV0c0M2sifQ==";
-app.get('/auth-1/*', (req, res, next) => {
-    res.cookie('session', cookie);
-    next();
-})
-app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
-app.use('/auth-1', express.static(path.join(__dirname, 'src/static/auth-1')));
-app.use('/auth-2', express.static(path.join(__dirname, 'src/static/auth-2')));
-app.use('/auth-3', express.static(path.join(__dirname, 'src/static/auth-3')));
+
+// app.use('/assets', express.static(path.join(__dirname, 'src/assets')));
+// app.use('/auth-1', express.static(path.join(__dirname, 'src/static/auth-1')));
+// app.use('/auth-2', express.static(path.join(__dirname, 'src/static/auth-2')));
+// app.use('/auth-3', express.static(path.join(__dirname, 'src/static/auth-3')));
 
 app.get('*', (req, res) => {
-    res.status(404).send("Not found!");
+    res.sendFile(index);
 });
 
 app.listen(port, () => {
