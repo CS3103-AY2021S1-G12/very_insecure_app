890cd1152a53cd5d92186edee0f53d714322bdcf
Huang Weijie <h_weijie@yahoo.com>
2020-11-08 15:11:19 +0800

    fix level 3 not sanitizing review input text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -489,15 +489,28 @@ let a = {
 							return;
 					}
 
-					fetch(`${window.location.href}\x77\x69\x6e\x63\x6f\x6e\x64\x69\x74\x69\x6f\x6e`);
+					// fetch(`${window.location.href}\x77\x69\x6e\x63\x6f\x6e\x64\x69\x74\x69\x6f\x6e`);
 					originalAlert(successText);
 					this.updatex('xss', i);
-			}
-			if (i == 2) {
+      }
+      
+      if (i === 1) {
+        let username = $("#username-field").text();
+        let inputText = $('#input'+i).val();
+        
+        const review = $('<div/>', { class: "review" });
+			
+        $(`<div class="username">${username}</div>`).appendTo(review);
+    
+        $(`<div class="text">${inputText}</div>`).appendTo(review);
+    
+        $('#reviews' + i).append(review);
+          
+      } else if (i === 2) {
 				let username = $("#username-field").text();
-
         let inputText = $('#input'+i).val();
         inputText = inputText.replace(/<script>/gi, "").replace(/<\/script>/gi, "");
+
         const review = $('<div/>', { class: "review" });
     
         $(`<div class="username">${username}</div>`).appendTo(review);
@@ -506,20 +519,22 @@ let a = {
     
         $('#reviews2').append(review);
 			} else {
-				let username = $("#username-field").text();
-				let inputText = $('#input'+i).val();
-					const review = $('<div/>', { class: "review" });
+        let username = $("#username-field").text();
+        let inputText = $('#input'+i).val();
+        
+        const review = $('<div/>', { class: "review" });
 			
-					$(`<div class="username">${username}</div>`).appendTo(review);
-			
-					$(`<div class="text">${inputText}</div>`).appendTo(review);
-			
-					$('#reviews' + i).append(review);
-			}
+        $(`<div class="username">${username}</div>`).appendTo(review);
 
+        $('<div/>', {
+            text: inputText,
+            class: "text"
+        }).appendTo(review);
+        
+        $('#reviews' + i).append(review);
+			}
 		},
 		changeusername : function () {
-
         let username = $("#username-input").val();
         $("#username-field").text(username || "Guest");
 		},
